46bdd7c06c5d3681b0adc6358e76651a53dd471c
Annylory LR <annyloryrosa@gmail.com>
2025-04-28 19:28:05 -0300

    schemas

---
+++ src/schemas/credentials-schema.ts
@@ -0,0 +1,9 @@
+import joi from 'joi';
+import { Credential } from '../../generated/prisma';
+
+export const credentialSchema = joi.object<Credential>({
+    title: joi.string().required(),
+    url: joi.string().required(),
+    username: joi.string().required(),
+    password: joi.string().required()
+});
