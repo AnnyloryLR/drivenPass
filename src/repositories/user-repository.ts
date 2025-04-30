dc5de17f83a66f087f7dbd37fc4a082ac0603f42
Annylory LR <annyloryrosa@gmail.com>
2025-04-30 14:43:26 -0300

    user repository

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,11 +1,13 @@
 import prisma from 'database/config';
 import { User } from '../../generated/prisma';
+import bcrypt from 'bcrypt';
 
 type UserData = Omit<User,"id">;
 
-export async function getUserEmail(userData:UserData){
-    const { email } = userData;
+type SignInData = Omit<User, "id" & "name">;
 
+export async function getUserData(email:string){
+   
     const result = await prisma.user.findFirst({
         where:{
             email: email
@@ -15,16 +17,30 @@ export async function getUserEmail(userData:UserData){
     return result;
 }
 
-export async function createUser(userData:UserData){
-    const { name, email, password } = userData;
+export async function signUp(userData:UserData){
+    const { name, email, password } = userData; 
     
     const user = await prisma.user.create({
         data:{
             name,
             email,
-            password
+            password: bcrypt.hashSync(password, 10)
         }
     });
 
     return user;
+}
+
+export async function signIn(signInData:SignInData){
+    const { email, password } = signInData;
+
+    const registeredUser = await prisma.user.findFirst({
+        where:{
+            email: email
+        }
+    })
+
+    const logIn = bcrypt.compareSync(registeredUser.password, password);
+
+    return logIn;
 }
