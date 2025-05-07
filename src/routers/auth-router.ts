756f93aab29cb50b3887f4ba5369571ada997f48
Annylory LR <annyloryrosa@gmail.com>
2025-05-07 18:15:23 -0300

    routers update

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -1,10 +1,11 @@
+import { signIn, signUp } from 'controllers/users-controller';
 import { Router } from 'express';
 import { schemaValidate } from 'middlewares/schema-middleware';
 import { signInSchema, signUpSchema } from 'schemas/user-schema';
 
 const authenticationRouter = Router();
 
-authenticationRouter.post("/sign-up", schemaValidate(signUpSchema));
-authenticationRouter.post("/sign-in", schemaValidate(signInSchema));
+authenticationRouter.post("/sign-up", schemaValidate(signUpSchema), signUp);
+authenticationRouter.post("/sign-in", schemaValidate(signInSchema), signIn);
 
 export default authenticationRouter;
--- src/routers/credentials-router.ts
+++ src/routers/credentials-router.ts
@@ -1,3 +1,4 @@
+import { credentialUpdate, deleteCredential, getAllCredentials, getCredentialById, newCredential } from 'controllers/credentials-controller';
 import { Router } from 'express';
 import { tokenValidate } from 'middlewares/auth-middleware';
 import { schemaValidate } from 'middlewares/schema-middleware';
@@ -6,9 +7,10 @@ import { credentialSchema } from 'schemas/credentials-schema';
 const credentialsRouter = Router();
 
 credentialsRouter.use(tokenValidate)
-credentialsRouter.post("/credentials", schemaValidate(credentialSchema));
-credentialsRouter.get("/credentials");
-credentialsRouter.put("/credentials/:id", schemaValidate(credentialSchema));
-credentialsRouter.delete("/credentials/:id");
+credentialsRouter.post("/credentials", schemaValidate(credentialSchema), newCredential);
+credentialsRouter.get("/credentials", getAllCredentials);
+credentialsRouter.get("/credentials/:id", getCredentialById);
+credentialsRouter.put("/credentials/:id", schemaValidate(credentialSchema), credentialUpdate);
+credentialsRouter.delete("/credentials/:id", deleteCredential);
 
 export default credentialsRouter;
